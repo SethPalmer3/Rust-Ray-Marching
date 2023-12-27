74e5482921b1fdf99c99a607a7797eb120b034f3
SethPalmer3 <elderlord30@gmail.com>
2023-12-26 21:19:18 -0800

    Formatted two files

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,53 @@
 pub mod ray_marcher;
 
-use ray_marcher::{threed_data_types::{Point, Direction as Vector3D}, scene_objects::{objects::Sphere, SurfaceMaterial}, color_data_types::Color, marcher};
 use image::{self, RgbImage};
+use ray_marcher::{
+    color_data_types::Color,
+    marcher,
+    scene_objects::{objects::Sphere, SurfaceMaterial},
+    threed_data_types::{Direction as Vector3D, Point},
+};
 
 fn main() {
     let height_res = 600;
     let width_res = 600;
-    let camera = ray_marcher::camera::Camera::new(Point::new(0.0, 0.0, 0.0), Vector3D::new(1.0, 0.0, 0.0).get_norm(), 1.0, 60_f64.to_radians(), (width_res, height_res));
-    let mut march_handler = ray_marcher::marcher::MarcherHandler::new(1000, marcher::MAX_DISTANCE, 50, camera);
+    let camera = ray_marcher::camera::Camera::new(
+        Point::new(0.0, 0.0, 0.0),
+        Vector3D::new(1.0, 0.0, 0.0).get_norm(),
+        1.0,
+        60_f64.to_radians(),
+        (width_res, height_res),
+    );
+    let mut march_handler =
+        ray_marcher::marcher::MarcherHandler::new(1000, marcher::MAX_DISTANCE, 50, camera);
     // march_handler.debug = true;
-    march_handler.add_scene_object(Sphere::new(Point::new(30.0, -10.0, 0.0), 10.0, Some(SurfaceMaterial{ color: Color::new(1.0, 0.0, 0.0), reflectivity: 1.0 })));
-    march_handler.add_scene_object(Sphere::new(Point::new(30.0, 10.0, 0.0), 10.0, Some(SurfaceMaterial{ color: Color::new(0.0, 0.0, 1.0), reflectivity: 0.0 })));
+    march_handler.add_scene_object(Sphere::new(
+        Point::new(30.0, -10.0, 0.0),
+        10.0,
+        Some(SurfaceMaterial {
+            color: Color::new(1.0, 0.0, 0.0),
+            reflectivity: 1.0,
+        }),
+    ));
+    march_handler.add_scene_object(Sphere::new(
+        Point::new(30.0, 10.0, 0.0),
+        10.0,
+        Some(SurfaceMaterial {
+            color: Color::new(0.0, 0.0, 1.0),
+            reflectivity: 0.0,
+        }),
+    ));
     march_handler.march();
 
-    let mut image_buf: RgbImage = image::ImageBuffer::new(march_handler.get_camera().resolution.0, march_handler.get_camera().resolution.1);
+    let mut image_buf: RgbImage = image::ImageBuffer::new(
+        march_handler.get_camera().resolution.0,
+        march_handler.get_camera().resolution.1,
+    );
 
-    for (x,y,pixel) in image_buf.enumerate_pixels_mut(){
+    for (x, y, pixel) in image_buf.enumerate_pixels_mut() {
         let col = march_handler.get_color(x, y);
         let (r, g, b) = col.get_u8_components();
         *pixel = image::Rgb([r, g, b]);
     }
     image_buf.save("ray_marched.png").unwrap();
-
 }
--- src/ray_marcher/scene_objects/objects.rs
+++ src/ray_marcher/scene_objects/objects.rs
@@ -1,10 +1,8 @@
-use crate::ray_marcher::color_data_types::Color;
-
 // use std::cmp;
-use super::{Point3D, SurfaceMaterial, SceneObject, DEFAULT_SURFACEMAT};
+use super::{Point3D, SceneObject, SurfaceMaterial, DEFAULT_SURFACEMAT};
 
 #[derive(Clone)]
-pub struct Sphere{
+pub struct Sphere {
     radius: f64,
     position: Point3D,
     material: SurfaceMaterial,
@@ -12,11 +10,13 @@ pub struct Sphere{
 
 #[allow(dead_code)]
 impl Sphere {
-
     pub fn new(pos: Point3D, radius: f64, sm: Option<SurfaceMaterial>) -> Self {
-        Sphere {radius, position: pos, material: sm.unwrap_or(DEFAULT_SURFACEMAT) }
+        Sphere {
+            radius,
+            position: pos,
+            material: sm.unwrap_or(DEFAULT_SURFACEMAT),
+        }
     }
-    
 }
 
 impl SceneObject for Sphere {
@@ -35,55 +35,75 @@ impl SceneObject for Sphere {
 
 #[cfg(test)]
 mod test {
-    use super::*;
     use super::super::Vector3D;
+    use super::*;
 
     #[test]
-    fn test_sphere_sdf_x(){
+    fn test_sphere_sdf_x() {
         let s = Sphere::new(Point3D::new(0.0, 0.0, 0.0), 1.0, None);
         let p = Point3D::new(2.0, 0.0, 0.0);
         assert_eq!(s.signed_distance(&p), 1.0);
     }
     #[test]
-    fn test_sphere_sdf_y(){
+    fn test_sphere_sdf_y() {
         let s = Sphere::new(Point3D::new(0.0, 0.0, 0.0), 1.0, None);
         let p = Point3D::new(0.0, 2.0, 0.0);
         assert_eq!(s.signed_distance(&p), 1.0);
     }
     #[test]
-    fn test_sphere_sdf_z(){
+    fn test_sphere_sdf_z() {
         let s = Sphere::new(Point3D::new(0.0, 0.0, 0.0), 1.0, None);
         let p = Point3D::new(0.0, 0.0, 2.0);
         assert_eq!(s.signed_distance(&p), 1.0);
     }
     #[test]
-    fn test_sphere_sdf_inner(){
+    fn test_sphere_sdf_inner() {
         let s = Sphere::new(Point3D::new(0.0, 0.0, 0.0), 1.0, None);
         let p = Point3D::new(0.0, 0.0, 0.0);
         assert_eq!(s.signed_distance(&p), -1.0);
     }
     #[test]
-    fn test_surface_normal_x(){
+    fn test_surface_normal_x() {
         let s = Sphere::new(Point3D::new(0.0, 0.0, 0.0), 1.0, None);
         let p = Point3D::new(1.0, 0.0, 0.0);
-        assert!((Vector3D::new(1.0, 0.0, 0.0) - s.get_surface_normal(&p, 0.0001)).length().abs() < 0.001);
+        assert!(
+            (Vector3D::new(1.0, 0.0, 0.0) - s.get_surface_normal(&p, 0.0001))
+                .length()
+                .abs()
+                < 0.001
+        );
     }
     #[test]
-    fn test_surface_normal_y(){
+    fn test_surface_normal_y() {
         let s = Sphere::new(Point3D::new(0.0, 0.0, 0.0), 1.0, None);
         let p = Point3D::new(0.0, 1.0, 0.0);
-        assert!((Vector3D::new(0.0, 1.0, 0.0) - s.get_surface_normal(&p, 0.0001)).length().abs() < 0.001);
+        assert!(
+            (Vector3D::new(0.0, 1.0, 0.0) - s.get_surface_normal(&p, 0.0001))
+                .length()
+                .abs()
+                < 0.001
+        );
     }
     #[test]
-    fn test_surface_normal_z(){
+    fn test_surface_normal_z() {
         let s = Sphere::new(Point3D::new(0.0, 0.0, 0.0), 1.0, None);
         let p = Point3D::new(0.0, 0.0, 1.0);
-        assert!((Vector3D::new(0.0, 0.0, 1.0) - s.get_surface_normal(&p, 0.0001)).length().abs() < 0.001);
+        assert!(
+            (Vector3D::new(0.0, 0.0, 1.0) - s.get_surface_normal(&p, 0.0001))
+                .length()
+                .abs()
+                < 0.001
+        );
     }
     #[test]
-    fn test_surface_normal_xy(){
+    fn test_surface_normal_xy() {
         let s = Sphere::new(Point3D::new(0.0, 0.0, 0.0), 1.0, None);
         let p = Point3D::new(1.0, 1.0, 0.0);
-        assert!((Vector3D::new(1.0, 1.0, 0.0).get_norm() - s.get_surface_normal(&p, 0.0001)).length().abs() < 0.001);
+        assert!(
+            (Vector3D::new(1.0, 1.0, 0.0).get_norm() - s.get_surface_normal(&p, 0.0001))
+                .length()
+                .abs()
+                < 0.001
+        );
     }
 }
